b7592004a72530a68477415d5286f70a41b20ffc
Krzysztof Urbanski <albiszon@gmail.com>
2016-10-13 21:03:52 +0200

    Update mutex.c

--- mutex-linux/mutex.c
+++ mutex-linux/mutex.c
@@ -8,11 +8,11 @@ void *th1(void *arg)
 {
 	for(;;)
 	{
-		pthread_mutex_lock (&mutex);
+		//pthread_mutex_lock (&mutex);
 		usleep(100); putchar('a'); fflush(stdout);
 		usleep(100); putchar('b'); fflush(stdout);
 		usleep(100); putchar('c'); fflush(stdout);
-		pthread_mutex_unlock (&mutex);
+		//pthread_mutex_unlock (&mutex);
 	}
 	return (NULL);
 }
@@ -21,11 +21,11 @@ void *th2(void *arg)
 {
 	for(;;)
 	{
-		pthread_mutex_lock (&mutex);
+		//pthread_mutex_lock (&mutex);
 		usleep(100); putchar('x'); fflush(stdout);
 		usleep(100); putchar('y'); fflush(stdout);
 		usleep(100); putchar('z'); fflush(stdout);
-		pthread_mutex_unlock (&mutex);
+		//pthread_mutex_unlock (&mutex);
 	}
 	return (NULL);
 }
@@ -35,7 +35,7 @@ int main() {
 	pthread_mutex_init (&mutex, NULL);
 	pthread_create(&a, NULL, th1, NULL);
 	pthread_create(&b, NULL, th2, NULL);
-	sleep(10);
+	sleep(5);
 	pthread_mutex_destroy(&mutex);
 	return 0;
 }
